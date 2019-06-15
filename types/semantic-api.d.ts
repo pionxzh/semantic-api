0050e641ab0540f0211f8ffcee2a6bf31de0be17
Pionxzh <spigbbbbb@gmail.com>
2019-06-15 17:25:24 +0800

    Update TS definition

--- types/semantic-api.d.ts
+++ types/semantic-api.d.ts
@@ -1,17 +1,21 @@
-interface customFunc {
-    (args: any[], calls: any[], url: string): void;
+declare module 'semantic-api' {
+    interface customFunc {
+        (args: any[], calls: any[], url: string): void;
+    }
+    interface customMethods {
+        [propName: string]: customFunc;
+    }
+    interface queryOption {
+        [propName: string]: any;
+    }
+    const checkProxySupport: () => void;
+    const isReflectorMethod: (methodName: string) => boolean;
+    const query: (args: any[], calls: any[], url: string) => void;
+    /**
+     * @param {string} baseUrl the baseUrl that will be added at the start of url
+     * @param {object} customFunctions key-value custom method function
+     */
+    function SemanticApi(baseUrl?: string, customFunctions?: customMethods): Function;
+
+    export default SemanticApi
 }
-interface customMethods {
-    [propName: string]: customFunc;
-}
-interface queryOption {
-    [propName: string]: any;
-}
-declare const checkProxySupport: () => void;
-declare const isReflectorMethod: (methodName: string) => boolean;
-declare const query: (args: any[], calls: any[], url: string) => void;
-/**
- * @param {string} baseUrl the baseUrl that will be added at the start of url
- * @param {object} customFunctions key-value custom method function
- */
-declare function SemanticApi(baseUrl?: string, customFunctions?: customMethods): Function;
